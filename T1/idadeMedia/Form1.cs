069d3206f20df25cc5cbd407543adafdea4a866b
GuilhermeLamim <guilherme_lamim@hotmail.com>
2018-07-16 14:30:55 -0300

    Calculando idade média

---
+++ T1/idadeMedia/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace idadeMedia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int aline = 40;
+            int silvana = 45;
+            int juliana = 11;
+
+            double idadeMedia = (aline + silvana + juliana) / 3;
+
+            MessageBox.Show("A idade média é: " + idadeMedia);
+        }
+    }
+}
