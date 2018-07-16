0e8cd4607d9d718e1de80ab5e6b1f4ab815e5de5
GuilhermeLamim <guilherme_lamim@hotmail.com>
2018-07-16 20:02:17 -0300

    Transferencia: método Saca, Deposito e Transferencia

---
+++ T1/CriandoClassTransferencia/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CriandoClassTransferencia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta Guilherme = new Conta(); 
+            Conta Mauricio = new Conta();
+
+            Guilherme.saldo = 2000.0;
+            Mauricio.saldo = 3000.0;
+
+            Guilherme.Transferencia(500.0, Mauricio);
+
+            MessageBox.Show("O saldo do Guilheme é de: " + Guilherme.saldo);
+            MessageBox.Show("O saldo do Mauricio é de: " + Mauricio.saldo);
+            
+        }
+    }
+}
