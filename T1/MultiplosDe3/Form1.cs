8439c1ca6c900aaa314d4f8e71574d13bb1199b6
GuilhermeLamim <guilherme_lamim@hotmail.com>
2018-07-16 15:49:46 -0300

    Os números que são multiplos de 3 de 1 a 100.

---
+++ T1/MultiplosDe3/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiplosDe3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int divi = 1;
+
+            for (int i = 1; i < 100; i++)
+            {
+                {
+                    if (i % 3 == 0)
+                    {
+                        MessageBox.Show("O número " + i + " é MULTIPLO de 3.");
+                    }
+                }
+            }
+
+        }
+    }
+}
