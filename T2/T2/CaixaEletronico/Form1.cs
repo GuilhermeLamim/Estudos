b138eea9daba453e331010a70353b05e07571fbd
GuilhermeLamim <guilherme_lamim@hotmail.com>
2018-07-17 17:40:10 -0300

    TextBox: convertendo int pra string e imprimindo na TextBox

---
+++ T2/T2/CaixaEletronico/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textoTitular_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Conta conta = new Conta();
+            conta.titular = "Vitor";
+            conta.numero = 1;
+            conta.saldo = 250.0;
+
+            string texto = Convert.ToString(conta.numero);
+
+            textoTitular.Text = conta.numero;
+        }
+    }
+}
