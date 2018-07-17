b4d90aabf418ea7ef1cd0a9c685cd2814a3d45fb
GuilhermeLamim <guilherme_lamim@hotmail.com>
2018-07-17 14:06:54 -0300

    Sacando com limites

---
+++ T1/CriandoClassCliente/Conta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoClassCliente
+{
+    class Conta
+    {
+        public int numero;
+        public double saldo;
+        public Cliente titular;
+        public int idade;
+
+        public bool maiorDeIdade()
+        {
+            return this.idade >= 18;
+        }
+        public bool Saca(double valorASerSacado)
+        {
+            if (valorASerSacado > this.saldo || valorASerSacado < 0)
+            {
+                return false;
+            }
+            else
+            {
+                if (this.titular.EhMaiorDeIdade())
+                {
+                    this.saldo -= valorASerSacado;
+                    return true;
+                }
+                else
+                {
+                    //novo
+                    if (valorASerSacado <= 200)
+                    {
+                        this.saldo -= valorASerSacado;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
+                    
