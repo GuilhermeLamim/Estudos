a5632653e7161315c5f6c3d03694626b756a149d
GuilhermeLamim <guilherme_lamim@hotmail.com>
2018-07-18 16:20:13 -0300

    Criando formulário e um novo botão 'textoValor'.

---
+++ T2/T2/CaixaEletronicoDeposito/Conta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronicoDeposito
+{
+    class Conta
+    {
+        public double Saldo { get; set; }
+        public int Numero { get; set; }
+        public Cliente Titular;
+
+
+
+        public void Deposito(double valor)
+        {
+            this.Saldo += valor;
+        }
+    }
+}
---
+++ T2/T2/CaixaEletronicoDeposito/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronicoDeposito
+{
+    public partial class Form1 : Form
+    {
+        private Conta conta;
+        private Cliente cliente;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.conta = new Conta();
+            cliente = new Cliente();
+            cliente.Nome = "Victor";
+            this.conta.Titular = cliente;
+            this.conta.Numero = 1;
+            this.conta.Deposito(250.0);
+
+            this.MostraConta();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textoDoValorDoDeposito = textoValor.Text;
+            double valorDeposito = Convert.ToDouble(textoDoValorDoDeposito);
+            this.conta.Deposito(valorDeposito);
+
+            this.MostraConta();
+        }
+
+        private void MostraConta()
+        {
+            textoNumero.Text = Convert.ToString(this.conta.Numero);
+            textoSaldo.Text = Convert.ToString(this.conta.Saldo);
+            textoTitular.Text = this.conta.Titular.Nome;
+        }
+    }
+}
