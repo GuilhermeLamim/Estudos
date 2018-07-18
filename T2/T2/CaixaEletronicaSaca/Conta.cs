168b5ac2bdf6c448415b9312729cdfd63884e493
GuilhermeLamim <guilherme_lamim@hotmail.com>
2018-07-18 19:06:03 -0300

    Saca e deposita mostrando o saldo(Deposito não funciona)

---
+++ T2/T2/CaixaEletronicaSaca/Conta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronicaSaca
+{
+    class Conta
+    {
+        public int Numero { get; set; }
+        public double Saldo { get; set; }
+        public Cliente Titular;
+
+        public void Deposito( double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public void Saca(double valor)
+        {
+            this.Saldo -= valor;
+        }
+    }
+}
---
+++ T2/T2/CaixaEletronicaSaca/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronicaSaca
+{
+    public partial class Form1 : Form
+    {
+        private Conta conta;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.conta = new Conta();
+            this.conta.Titular = new Cliente();
+
+            this.conta.Titular.Nome = "Victor";
+            this.conta.Numero = 1;
+            this.conta.Deposito(250.0);
+
+            this.MostraConta();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string textoDoValorDoDeposito = textoValor.Text;
+            double valorDeposito = Convert.ToDouble(textoDoValorDoDeposito);
+            this.conta.Deposito(valorDeposito);
+
+            this.MostraConta();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string textoDoValorDoSaque = textoValor.Text;
+            double valorSaque = Convert.ToDouble(textoDoValorDoSaque);
+            this.conta.Saca(valorSaque);
+
+            this.MostraConta();
+        }
+
+        private void MostraConta()
+        {
+            textoNumero.Text = Convert.ToString(this.conta.Numero);
+            textoSaldo.Text = Convert.ToString(this.conta.Saldo);
+            textoTitular.Text = this.conta.Titular.Nome;
+        }
+
+        
+    }
+}
