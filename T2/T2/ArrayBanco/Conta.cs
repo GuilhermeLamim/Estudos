e2bc5f9303103786a050f422a09d383f8f4bcc0e
GuilhermeLamim <guilherme_lamim@hotmail.com>
2018-07-19 15:01:45 -0300

    Criando array com duas posições e atribuindo Nome,Titular e Saldo no ComboBox e imprimindo no textBox

---
+++ T2/T2/ArrayBanco/Conta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayBanco
+{
+    class Conta
+    {
+        public double Saldo { get; set; }
+       // public Cliente Titular;
+
+        public void Deposito(double valor)
+        {
+            this.Saldo += valor;
+        }
+    }
+}
---
+++ T2/T2/ComboBox/Conta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboBox
+{
+    class Conta
+    {
+        public double Saldo;
+        public string Titular;
+        public int numero;
+       // public Cliente Titular;
+
+    }
+}
