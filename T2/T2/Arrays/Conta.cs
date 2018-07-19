4b97adc41e31027b69168daef6030a2b73bfd7e6
GuilhermeLamim <guilherme_lamim@hotmail.com>
2018-07-18 23:58:01 -0300

    Arrays com string usando o foreach

---
+++ T2/T2/Arrays/Conta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Conta
+    {
+        public double Numero;
+        public Double Saldo;
+        public void Deposita(double valor)
+        {
+            this.Saldo += valor;
+        }
+    }
+}
